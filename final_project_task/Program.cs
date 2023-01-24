34f94a42091b525b6de40032e0df033a89f9ed9e
Igor Zhidkov <stork32@mail.ru>
2023-01-24 14:32:52 +0300

    add solution

---
+++ final_project_task/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] strArray = { "hello", "2", "world", ":-)" };
+int count = 0;
+int index = 0;
+string temporary = string.Empty;
+int i;
+
+for (i = 0; i < strArray.Length; i++)
+{
+    if (strArray[i].Length <= 3)
+    {
+        temporary = strArray[index];
+        strArray[index] = strArray[i];
+        strArray[i] = temporary;
+        count++;
+        index++;
+    }
+}
+
+string[] newStrArray = new string[count];
+
+for (i = 0; i < newStrArray.Length; i++)
+{
+    newStrArray[i] = strArray[i];
+}
+
+for (i = 0; i < newStrArray.Length; i++)
+{
+    Console.Write(newStrArray[i] + " ");
+}
